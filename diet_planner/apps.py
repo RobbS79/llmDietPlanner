1c074c40eb12d1c243826d219c703e568f56c7e2
robertsoroka <soroka.robert8@gmail.com>
2025-12-31 15:03:37 +0100

    Fix: Register signal handlers in DietPlannerConfig.ready()

--- diet_planner/apps.py
+++ diet_planner/apps.py
@@ -1,6 +1,15 @@
+"""
+App configuration for diet_planner.
+"""
 from django.apps import AppConfig
 
 
 class DietPlannerConfig(AppConfig):
+    """Configuration for diet_planner app."""
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'diet_planner'
+    
+    def ready(self):
+        """Import signal handlers when app is ready."""
+        import diet_planner.signals  # noqa
